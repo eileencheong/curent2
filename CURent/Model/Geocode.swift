a58ea317449c4b72bcbcb4a363989b4757397805
Eileen Cheong <eyc47@cornell.edu>
2018-05-02 15:44:03 -0400

    trying

--- CURent/Model/Geocode.swift
+++ CURent/Model/Geocode.swift
@@ -1,21 +1,21 @@
+////
+////  Geocode.swift
+////  CURent
+////
+////  Created by Jaewon Sim on 5/1/18.
+////  Copyright © 2018 CURent. All rights reserved.
+////
 //
-//  Geocode.swift
-//  CURent
+//import Foundation
+//import Alamofire
+//import SwiftyJSON
 //
-//  Created by Jaewon Sim on 5/1/18.
-//  Copyright © 2018 CURent. All rights reserved.
+//class Geocode {
+//    private static let endpoint = "https://maps.googleapis.com/maps/api/geocode/json"
+//    private static let key = "AIzaSyDbAj9YaE9T-QeCj7Q2gqTm2Nz4O-TUQ08"
 //
-
-import Foundation
-import Alamofire
-import SwiftyJSON
-
-class Geocode {
-    private static let endpoint = "https://maps.googleapis.com/maps/api/geocode/json"
-    private static let key = "AIzaSyDbAj9YaE9T-QeCj7Q2gqTm2Nz4O-TUQ08"
-
-    static func geocode(withQuery query: String, _ completion: @escaping ([Recipe]) -> Void) {
-
-    }
-
-}
+//    static func geocode(withQuery query: String, _ completion: @escaping ([Recipe]) -> Void) {
+//
+//    }
+//
+//}
