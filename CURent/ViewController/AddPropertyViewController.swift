09a11c43a2f668db25ad8805d9a1c4b4a5e1aa2c
Jaewon Sim <js2689@cornell.edu>
2018-05-02 16:47:46 -0400

    Partial fix for add VC

--- CURent/ViewController/AddPropertyViewController.swift
+++ CURent/ViewController/AddPropertyViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import SnapKit
 
 class AddPropertyViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, GMSAutocompleteViewControllerDelegate {
 
@@ -23,6 +24,8 @@ class AddPropertyViewController: UIViewController, UIImagePickerControllerDelega
     var propertyAddressField: UITextField!
     var propertyDescriptionView: UITextView!
     
+    let autocompleteViewController = GMSAutocompleteViewController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +38,12 @@ class AddPropertyViewController: UIViewController, UIImagePickerControllerDelega
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(closeView))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveProperty))
         
-        
-        let autocompleteViewController = GMSAutocompleteViewController()
+       
         autocompleteViewController.delegate = self
 
         propertyImageView = UIImageView()
+        propertyImageView.layer.masksToBounds = true
+        propertyImageView.layer.cornerRadius = 10
         propertyImageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(propertyImageView)
         
@@ -47,30 +51,36 @@ class AddPropertyViewController: UIViewController, UIImagePickerControllerDelega
         propertyImageChangeButton.setTitle("Change image", for: .normal)
         propertyImageChangeButton.setTitleColor(.gray, for: .normal)
         propertyImageChangeButton.addTarget(self, action: #selector(photoFromLibrary(_sender:)), for: .touchUpInside)
+        propertyImageChangeButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         propertyImageChangeButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(propertyImageChangeButton)
         
         
         propertyNameField = UITextField()
         propertyNameField.borderStyle = .roundedRect
+        propertyNameField.font = UIFont.systemFont(ofSize: 12)
         propertyNameField.placeholder = "Property name"
         propertyNameField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(propertyNameField)
         
         propertyPriceField = UITextField()
         propertyPriceField.borderStyle = .roundedRect
+        propertyPriceField.font = UIFont.systemFont(ofSize: 12)
         propertyPriceField.placeholder = "Price"
         propertyPriceField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(propertyPriceField)
         
         propertyAddressField = UITextField()
         propertyAddressField.borderStyle = .roundedRect
+        propertyAddressField.font = UIFont.systemFont(ofSize: 12)
         propertyAddressField.placeholder = "Address"
+        propertyAddressField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingDidBegin)
         propertyAddressField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(propertyAddressField)
         
         propertyDescriptionView = UITextView()
         propertyDescriptionView.text = "Description"
+        propertyDescriptionView.font = UIFont.systemFont(ofSize: 12)
         propertyDescriptionView.layer.cornerRadius = 10
         propertyDescriptionView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(propertyDescriptionView)
@@ -110,27 +120,30 @@ class AddPropertyViewController: UIViewController, UIImagePickerControllerDelega
         NSLayoutConstraint.activate([
             propertyAddressField.leadingAnchor.constraint(equalTo: propertyNameField.leadingAnchor),
             propertyAddressField.trailingAnchor.constraint(equalTo: propertyNameField.trailingAnchor),
-            propertyAddressField.topAnchor.constraint(equalTo: propertyNameField.bottomAnchor, constant: 18)
+            propertyAddressField.topAnchor.constraint(equalTo: propertyPriceField.bottomAnchor, constant: 18)
             ])
         
         NSLayoutConstraint.activate([
             propertyDescriptionView.leadingAnchor.constraint(equalTo: propertyNameField.leadingAnchor),
             propertyDescriptionView.trailingAnchor.constraint(equalTo: propertyNameField.trailingAnchor),
-            propertyAddressField.topAnchor.constraint(equalTo: propertyAddressField.bottomAnchor, constant: 18)
+            propertyDescriptionView.topAnchor.constraint(equalTo: propertyAddressField.bottomAnchor, constant: 18)
             ])
     }
     
-    func openAutocompleteViewController() {
-        self.present(autocompleteViewController, animated: true,  completion: nil)
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        present(autocompleteViewController, animated: true,  completion: nil)
     }
     
     func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-        
+        propertyAddressField.text = place.formattedAddress
         dismiss(animated: true, completion: nil)
     }
     
     func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
-        <#code#>
+        let autocompleteErrorAlert = UIAlertController(title: "Error", message: "An error occurred while autocompleting your address. Please try again.", preferredStyle: .alert)
+        autocompleteErrorAlert.addAction(UIAlertAction(title: "Go back", style: .cancel, handler: { action in
+            autocompleteErrorAlert.dismiss(animated: true, completion: nil)
+        }))
     }
     
     func wasCancelled(_ viewController: GMSAutocompleteViewController) {
